869dcf034b8761db24c9a9e5bd9a6bac794b8250
Caroline E <caroline.esquivel01@gmail.com>
2021-04-08 00:24:17 -0500

    added useEffect hook and installed axis

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
 const Search = () => {
     const [term, setTerm] = useState('');
+
+    useEffect(() => {
+
+    }, []);
 
     return  (
         <div>
