dbbdbc0e29cf7f94519d94102103d6b9f334f5b9
Caroline E <caroline.esquivel01@gmail.com>
2021-04-10 23:09:48 -0500

    added async axios request

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,12 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import axios from "axios";
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
 
     useEffect(() => {
+        const search = async () => {
+            await axios.get('asdf')
+        };
 
-    }, []);
+        search();
+    }, [term]);
 
     return  (
         <div>
