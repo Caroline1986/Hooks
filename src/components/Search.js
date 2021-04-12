0eafa3eaa51cde5f34bbf55099ee19ada4f550a9
Caroline E <caroline.esquivel01@gmail.com>
2021-04-11 22:04:24 -0500

    request initiated instantly

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
@@ -7,55 +7,68 @@ const Search = () => {
 
     useEffect(() => {
         const search = async () => {
-            const {data} = await axios.get('https://en.wikipedia.org/w/api/php', {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    sresearch: term,
+                    srsearch: term,
                 },
             });
 
             setResults(data.query.search);
         };
 
-        search();
+        if (term && !results.length){
+            search();
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (term) {
+                    search();
+                }
+            }, 1000);
+        }
+
+
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
     }, [term]);
 
     const renderedResults = results.map((result) => {
         return (
-            <div key={result.pageid} className={"item"}>
-                <div className={"right floated content"}>
-                    <a className={"ui button"}>Go</a>
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
                 </div>
-                <div
-                    className={"content"}
-                    href={`https://en.wikipedia.org?curid=${result.pageid}`}>
-                    <div className={"header"}>
-                        {result.title}
-                    </div>
+                <div className="content">
+                    <div className="header">{result.title}</div>
                     <span dangerouslySetInnerHTML={{__html: result.snippet}}/>
                 </div>
             </div>
-        )
-    })
+        );
+    });
 
     return (
         <div>
-            <div className={"ui form"}>
-                <div className={"field"}>
+            <div className="ui form">
+                <div className="field">
                     <label>Enter Search Term</label>
                     <input
                         value={term}
-                        onChange={e => setTerm(e.target.value)}
-                        className={"input"
-                        }/>
+                        onChange={(e) => setTerm(e.target.value)}
+                        className="input"
+                    />
                 </div>
             </div>
-            <div className={"ui celled list"}>
-                {renderedResults}
-            </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     );
 };
