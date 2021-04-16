f003fec9ba531a351a1e110d6f4075057152ccd8
Caroline E <caroline.esquivel01@gmail.com>
2021-04-16 00:04:05 -0500

    added cleanup function to remove event listner on body click

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -6,14 +6,20 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
 
 
     useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+
+            setOpen(false);
+        };
+
         document.body.addEventListener(
-            "click",
-            (event) => {
-                if (ref.current.contains(event.target)) {
-                    return;
-                }
-           setOpen(false);
-        });
+            "click", onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick)
+        };
     }, [])
 
     const renderedOptions = options.map((option) => {
