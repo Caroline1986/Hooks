1d0136fe8ea3cb8cdf91ad6476a08ab326db9ed4
Caroline E <caroline.esquivel01@gmail.com>
2021-04-17 23:26:18 -0500

    update setState

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -5,19 +5,29 @@ const Convert = ({language, text}) => {
     const [translated, setTranslated] = useState('');
 
     useEffect(() => {
-        axios.post('https://translation.googleapis.com/language/translate/v2',
-            {},
-            {
-                params: {
-                    q: text,
-                    target: language.value,
-                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-                },
-            }
-        );
+        const doTranslation = async () => {
+            const {data} = await axios.post(
+                'https://translation.googleapis.com/language/translate/v2',
+                {},
+                {
+                    params: {
+                        q: text,
+                        target: language.value,
+                        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+                    },
+                }
+            );
+
+            setTranslated(data.data.translations[0].translateText)
+        };
+        doTranslation();
     }, [language, text]);
 
-    return <div/>;
+    return (
+        <div>
+            <h1 className={"ui header"}>{translated}</h1>
+        <div/>
+    );
 };
 
 export default Convert;
