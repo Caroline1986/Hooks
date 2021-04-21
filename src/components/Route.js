b940c908caa06c2c53bd71aed325d354c0d1cd5b
Caroline E <caroline.esquivel01@gmail.com>
2021-04-20 23:04:16 -0500

    added usestate to update current path on location change

--- src/components/Route.js
+++ src/components/Route.js
@@ -2,9 +2,11 @@
 import {useEffect, useState} from "react";
 
 const Route = ({path, children}) => {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
     useEffect(() => {
         const onLocationChange = () => {
-
+            setCurrentPath(window.location.pathname);
         }
 
         window.addEventListener('popstate', onLocationChange);
