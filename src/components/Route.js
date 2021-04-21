b1ce950c3175e665679dac75c279aac400c31351
Caroline E <caroline.esquivel01@gmail.com>
2021-04-20 23:01:27 -0500

    added useEffect for on location change and event listners

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,6 +1,19 @@
 // import React from "react";
+import {useEffect, useState} from "react";
 
 const Route = ({path, children}) => {
+    useEffect(() => {
+        const onLocationChange = () => {
+
+        }
+
+        window.addEventListener('popstate', onLocationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange)
+        }
+    }, []);
+
     return window.location.pathname === path
     ? children
         : null;
